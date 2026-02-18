0af306f99b8327b0a385491d7de2b8c23bfa4c28
sonatard <sonatard12@gmail.com>
2026-02-19 00:18:34 +0900

    Use singlechecker

--- cmd/mustcalls/main.go
+++ cmd/mustcalls/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/nametake/mustcalls"
-	"golang.org/x/tools/go/analysis/unitchecker"
+	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
-func main() { unitchecker.Main(mustcalls.Analyzer) }
+func main() { singlechecker.Main(mustcalls.Analyzer) }
